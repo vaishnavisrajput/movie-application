2d1b98875b44a262b18a150a95d794fb39ea7b2c
“Prashant2415” <prashantmendhejbp@gmail.com>
2024-12-22 14:36:19 +0530

    Server side configuration

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./route");
+dotenv.config();
 require("./connection");
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(router);
 app.listen(port,()=>{
     console.log("Running server");
 })
--- server/connection.js
+++ server/connection.js
@@ -1,9 +1,12 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const connect = mysql.createConnection({
-    user: "root",
-    password: "P@th@2415finder",
-    host: "localhost",
-    database: "movieapplication"
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    database: process.env.DATABASE
 });
 connect.connect((error)=>{
     if (error) throw error;
